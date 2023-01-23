caa344f360a62335b0b3602f4b49f5e8b0a2e63a
Damlrca <sir.damirca@yandex.ru>
2023-01-23 23:46:18 +0300

    eng/rus

--- Calendar_Generator/main.cpp
+++ Calendar_Generator/main.cpp
@@ -11,21 +11,12 @@ inline constexpr int is_leap(const int x) {
 	return (x % 4 == 0 && x % 100 != 0) || x % 400 == 0;
 }
 
-int main() {
-	int this_year;
-	cout << "Input year: ";
-	cin >> this_year;
-	if (this_year < 1970 || this_year > 3000) {
-		cout << "expected 1970 - 3000";
-		return 0;
-	}
-	
-	string year_string = to_string(this_year);
-	string filename = "calendar_" + year_string + ".txt";
-
+void generate_calendar(int year, string filename, const wstring* months_names, const wstring* day_of_week) {
 	const locale utf8_locale = locale(locale(), new codecvt_utf8<wchar_t>());
 	wofstream fout(filename);
 	fout.imbue(utf8_locale);
+
+	string year_string = to_string(year);
 
 	for (int i = 0; i < 5; i++) {
 		for (char c : year_string) {
@@ -36,13 +27,14 @@ int main() {
 	fout << L"\n";
 
 	int day_now = 3;
-	for (int year = 1970; year < this_year; year++)
-		day_now = (day_now + 365 + is_leap(year)) % 7;
-	const int this_year_is_leap = is_leap(this_year);
+	for (int i = 1970; i < year; i++)
+		day_now = (day_now + 365 + is_leap(i)) % 7;
+	const int this_year_is_leap = is_leap(year);
+
 	for (int month = 0; month < 12; month++) {
-		fout << months_names_ru[month] << L"\n";
+		fout << months_names[month] << L"\n";
 		for (int i = 0; i < 7; i++)
-			fout << L" " << day_of_week_ru[i];
+			fout << L" " << day_of_week[i];
 		fout << L"\n";
 		int t = 1;
 		for (int i = 0; i < 3 * day_now; i++) fout << " ";
@@ -57,6 +49,40 @@ int main() {
 	}
 
 	fout.close();
+}
+
+int main() {
+	int this_year;
+	cout << "Input year (1970-3000): ";
+	cin >> this_year;
+	if (this_year < 1970 || this_year > 3000) {
+		cout << "expected 1970 - 3000";
+		return 0;
+	}
+	
+	string lang;
+	cout << "Language (eng/rus): ";
+	cin >> lang;
+	if (lang != "eng" && lang != "rus") {
+		cout << "expected \"eng\" or \"rus\"\n";
+		return 0;
+	}
+
+	string year_string = to_string(this_year);
+	string filename = "calendar_" + year_string;
+
+	if (lang == "eng") {
+		filename += "_eng.txt";
+		generate_calendar(this_year, filename, months_names, day_of_week);
+	}
+	else if (lang == "rus") {
+		filename += "_rus.txt";
+		generate_calendar(this_year, filename, months_names_ru, day_of_week_ru);
+	}
+	else {
+		cout << "expected \"eng\" or \"rus\"\n";
+		return 0;
+	}
 
 	cout << "calendar saved in file " << filename << "\n";
 
